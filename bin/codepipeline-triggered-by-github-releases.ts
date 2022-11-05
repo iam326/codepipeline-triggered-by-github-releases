05bce731207f60adc8b4c101d805e3ad0dc1ac5b
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2022-11-05 12:49:58 +0900

    add cicd stack

--- bin/codepipeline-triggered-by-github-releases.ts
+++ bin/codepipeline-triggered-by-github-releases.ts
@@ -4,18 +4,17 @@ import * as cdk from 'aws-cdk-lib';
 import { CodepipelineTriggeredByGithubReleasesStack } from '../lib/codepipeline-triggered-by-github-releases-stack';
 
 const app = new cdk.App();
-new CodepipelineTriggeredByGithubReleasesStack(app, 'CodepipelineTriggeredByGithubReleasesStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const projectName = app.node.tryGetContext('projectName');
+const githubOwnerName = app.node.tryGetContext('githubOwnerName');
+const githubRepositoryName = app.node.tryGetContext('githubRepositoryName');
+const githubBranchName = app.node.tryGetContext('githubBranchName');
+const githubTokenName = app.node.tryGetContext('githubTokenName');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CodepipelineTriggeredByGithubReleasesStack(app, `${projectName}-cicd`, {
+  projectName,
+  githubOwnerName,
+  githubRepositoryName,
+  githubBranchName,
+  githubTokenName,
 });
--- lib/codepipeline-triggered-by-github-releases-stack.ts
+++ lib/codepipeline-triggered-by-github-releases-stack.ts
@@ -1,16 +1,75 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as codeBuild from 'aws-cdk-lib/aws-codebuild';
+import * as codePipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codePipelineActions from 'aws-cdk-lib/aws-codepipeline-actions';
+
+export interface CodepipelineTriggeredByGithubReleaseStackProps
+  extends cdk.StackProps {
+  projectName: string;
+  githubOwnerName: string;
+  githubRepositoryName: string;
+  githubBranchName: string;
+  githubTokenName: string;
+}
 
 export class CodepipelineTriggeredByGithubReleasesStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: CodepipelineTriggeredByGithubReleaseStackProps
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const {
+      projectName,
+      githubOwnerName,
+      githubRepositoryName,
+      githubBranchName,
+      githubTokenName,
+    } = props;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodepipelineTriggeredByGithubReleasesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const githubToken =
+      cdk.SecretValue.secretsManager(githubTokenName).unsafeUnwrap();
+
+    const sourceArtifact = new codePipeline.Artifact();
+
+    const codeBuildDeployProject = new codeBuild.PipelineProject(
+      this,
+      'CodeBuildDeployProject',
+      {
+        projectName: `${projectName}-deploy-project`,
+        buildSpec: codeBuild.BuildSpec.fromSourceFilename('./buildspec.yml'),
+      }
+    );
+
+    const sourceAction = new codePipelineActions.GitHubSourceAction({
+      actionName: 'source',
+      owner: githubOwnerName,
+      repo: githubRepositoryName,
+      branch: githubBranchName,
+      oauthToken: new cdk.SecretValue(githubToken),
+      output: sourceArtifact,
+    });
+
+    const deployAction = new codePipelineActions.CodeBuildAction({
+      actionName: 'deploy',
+      project: codeBuildDeployProject,
+      input: sourceArtifact,
+    });
+
+    const deployPipeline = new codePipeline.Pipeline(this, 'DeployPipeline', {
+      pipelineName: `${projectName}-deploy-pipeline`,
+      stages: [
+        {
+          stageName: 'source',
+          actions: [sourceAction],
+        },
+        {
+          stageName: 'deploy',
+          actions: [deployAction],
+        },
+      ],
+    });
   }
 }
